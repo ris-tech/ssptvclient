7d6744897711a0113d06563b283073d280bb27e1
ris-tech <info@ris-tech.de>
2024-11-13 10:36:14 +0100

    conf

--- config/static_arrays.php
+++ config/static_arrays.php
@@ -2,14 +2,6 @@
 class StaticArray
 {
 
-    public static $datenames = [
-        '1' => 'Ponedeljak',
-        '2' => 'Utorak',
-        '3' => 'Sreda',
-        '4' => 'Četvrtak',
-        '5' => 'Petak',
-        '6' => 'Subota',
-        '7' => 'Nedelja'
-    ];
+    //
 
 }
